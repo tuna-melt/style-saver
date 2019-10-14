149f5ec65d572c96f674fac9c332fdf555977e65
Dante Tobar <dante.m.tobar@gmail.com>
2019-10-14 11:04:02 -0500

    initial commit

---
+++ views/components/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Main = () => {
+  return (
+    <div className="pair-container">
+      <div className="outer-block A">
+        <div className="inner-block a" />
+      </div>
+      <div className="outer-block B">
+        <div className="inner-block b" />
+      </div>
+    </div>
+  );
+};
+
+export default Main;
---
+++ views/components/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import Main from './Main';
+
+ReactDOM.render(<Main />, document.getElementById('app'));
